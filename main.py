bfcf960f01248123f265adeeb8af418286c3e8f0
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-02 18:48:49 +0800

    feat: Add chat messages
    - Imported streamlit_chat message function.
    - Added initial chat messages to the main page, one from ChatGPT and one from the user.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
 
 st.set_page_config(
     page_title = "Converse AI",
@@ -8,6 +10,9 @@ st.set_page_config(
 )
 
 st.header("Your own ChatGPT")
+
+message("Hello! I'm ChatGPT. How can I help you today?")
+message("Yooo! I'm gooooood!", is_user = True)
 
 with st.sidebar:
     user_input = st.text_input("Your message: ", key = "user_input")
